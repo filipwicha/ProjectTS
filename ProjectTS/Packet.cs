9dedd7b66c5a5cfdc1b7b859bd06654739c5c1da
Filip Wicha <5021567@gmail.com>
2017-11-27 20:31:24 +0100

    a

--- ProjectTS/Packet.cs
+++ ProjectTS/Packet.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 //wszystkie dane przesyłane w postaci binarnej,
 //pole operacji o długości 3 bitów,
 //pola liczb o długości 32 bitów,
-//pole statusu o długości 2 bitów,
+//pole statusu o długości 2 bitów, 
 //pole identyfikatora o długości 8 bitów,
 //dodatkowe pola zdefiniowane przez programistę
 
